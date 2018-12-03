0306f35ba8724c27a08cd263b2f2b91577b19548
sivakrishna <root@localhost.localdomain>
2018-12-03 10:59:11 +0000

    12022018

---
+++ python/firs.py
@@ -0,0 +1,10 @@
+#!/bin/python
+print 'Hello'
+print "Python"
+print "World"
+print """ I don't know anything about
+python, so I started learning."""
+myname= "Siva"
+myage= 30
+print myname
+print myage
