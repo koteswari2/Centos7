cb932bbc6cefe83a678b76ec12b8fd643ac87c5a
sivakrishna <root@localhost.localdomain>
2018-12-06 13:48:41 +0000

    fun_tuple

--- python/fun.py
+++ python/fun.py
@@ -1,15 +1,17 @@
 #!/bin/python
 
-def print_test():
-   print "Hello"
-   print "Bollo"
+def print_test(Hello,*vartuple):
+    print Hello
+    for var in vartuple:
+     print var
+print_test(12,2)
+print_test(1,2,3,4,5,6,7,8,9)
 
 def add_test(num1,num2):
    total = num1+num2
    return total
 
 
-print print_test()
 print add_test(2,5)
 
 def print_details(Name,Age=3):
@@ -28,3 +30,11 @@ def sivainfo(name,age=30):
   print "Age is %d"  %age
 
 sivainfo("siva",100)
+
+def tuple_example(apple,*vartuple):
+   print apple
+   for var in vartuple:
+    print var
+   return
+tuple_example(10)
+tuple_example(1,2,3,4,5,6,7)
