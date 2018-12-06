776e2dbfa3ff61579d4c208b541a7826c08fb779
sivakrishna <root@localhost.localdomain>
2018-12-06 12:59:30 +0000

    funnn

--- python/fun.py
+++ python/fun.py
@@ -11,3 +11,20 @@ def add_test(num1,num2):
 
 print print_test()
 print add_test(2,5)
+
+def print_details(Name,Age=3):
+   print "Name:",Name
+   print "Age",Age
+   return 
+print print_details("siva",43)
+
+def movie(story,direction):
+   movie=story + direction
+   return movie
+print movie("hari","vvv")
+
+def sivainfo(name,age=30):
+  print "Name is %s" %name
+  print "Age is %d"  %age
+
+sivainfo("siva",100)
