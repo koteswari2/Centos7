0f1c60e3b44cdc21bf5ff1e3fce4d1a66c48ceb5
sivakrishna <root@localhost.localdomain>
2018-12-06 12:21:39 +0000

    fn

---
+++ python/fun.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+def print_test():
+   print "Hello"
+   print "Bollo"
+
+def add_test(num1,num2):
+   total = num1+num2
+   return total
+
+
+print print_test()
+print add_test(2,5)
